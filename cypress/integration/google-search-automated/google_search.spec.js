a8e6b0222f474e187499c247b58c50da440c1434
christian.alvarez <christian.alvarez@sofka.com.co>
2023-06-25 10:41:42 -0500

    agregar detalle de escenario

--- cypress/integration/google-search-automated/google_search.spec.js
+++ cypress/integration/google-search-automated/google_search.spec.js
@@ -1,8 +1,12 @@
+
 describe("testing in google",()=>{
     beforeEach(()=>{
         cy.visit('https://www.google.com/');
     })
     it("make a search in google",()=>{
+        /*Escenario automatizado
+        Como usuario quiero buscar cualquier término utilizando la barra de búsqueda única en pantalla  
+        */
         const wordSearch= 'Historia de Colombia';
         cy.get('textarea[title=Buscar]').type(`${wordSearch}{enter}`);
         cy.get('div > a[href="https://es.wikipedia.org/wiki/Historia_de_Colombia"]')
